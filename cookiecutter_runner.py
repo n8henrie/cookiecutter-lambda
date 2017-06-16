219cf85b62c5c570c6750f54b48e561ae23734bd
Nathan Henrie <nate@n8henrie.com>
2017-06-15 22:35:20 -0600

    Initial commit

---
+++ cookiecutter_runner.py
@@ -0,0 +1,16 @@
+from cookiecutter.main import cookiecutter
+import datetime
+import os.path
+
+curdir = os.path.dirname(os.path.abspath(__file__))
+
+today = datetime.datetime.today()
+date = today.date().isoformat()
+year = today.year
+
+extra_context = {
+        'release_date': date,
+        'year': year
+        }
+
+cookiecutter(curdir, extra_context=extra_context)
